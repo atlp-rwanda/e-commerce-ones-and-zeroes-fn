3507fc47903548474c315d3ed918e5a2399e364c
Igor IHIMBAZWE <igorihimbazwe@Igors-MacBook-Pro.local>
2024-05-30 16:52:16 +0200

    Finishes [#187355038] setting up an empty react project

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Hello, Ones and Zeros Ecommerce !</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
