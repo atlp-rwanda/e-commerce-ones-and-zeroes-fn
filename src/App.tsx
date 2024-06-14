a232c578584e287c0cd38422ba5ba505d7aa2ca0
Celestin4 <nteziryayocelestin4@gmail.com>
2024-06-14 15:22:44 +0200

    [Finishes #187355049] user can register and verify email]

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './styles/index.scss';
 import Signup from './Pages/Signup/Signup';
-import Toast from './components/Toast/Toast';
 import DummyHomePage from './Pages/DummyHomePage/DummyHomePage';
 
 function App() {
@@ -14,12 +13,9 @@ function App() {
         <Route  path='/' element={<DummyHomePage />}/>
         <Route  path='/signup' element={<Signup />}/>
         
-      {/* <Task /> */}
-      {/* <TaskList /> */}
 
       </Routes>
       </BrowserRouter>
-      {/* <Toast /> */}
 
 
     </div>
