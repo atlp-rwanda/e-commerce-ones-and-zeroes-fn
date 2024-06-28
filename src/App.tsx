3723afb13cd2769342bb5c857b58c15fb7a4acb1
christian Ishimwe <christianinja3@gmail.com>
2024-06-28 11:18:14 +0200

    [Finishes #187355052] Users should be presented with a reset password #14

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import Login from './Pages/Login/Login'
 import Signup from './Pages/Signup/Signup';
 import IsVerified from './components/IsVerifiedModal/IsVerified';
 import UserVerificationFailed from './components/IsVerifiedModal/UserVerificationFailed';
-
+import ResetPassword from './Pages/resetPassword/ResetPassword';
+import ResetPasswordPage from './Pages/resetPassword/ResetPasswordPage';
 
 const App: React.FC = () => (
   <Router>
-    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route  path='/signup' element={<Signup />}/>
       <Route  path='/users/isVerified' element={<IsVerified />}/>
       <Route  path='/users/userVerifyFailed' element={<UserVerificationFailed />}/>
+      <Route path='/reset' element={<ResetPassword/>} />
+      <Route path='/reset/new-password' element={<ResetPasswordPage/>} />
     </Routes>
   </Router>
 );
